d2b139d4a47827ca859d76fffbc687da23af35ff
Gaetano Mondelli <gaetano.mondelli@gmail.com>
2024-04-13 13:25:48 +0100

    feat: build and run of validator and relayer

--- initialise_bridge.sh
+++ initialise_bridge.sh
@@ -45,6 +45,35 @@ hyperlane deploy core \
     --out $ART_FOLDER \
     --key $HYP_KEY  \
     --yes 
-
 fi
+
+
+
+export ARG_FILE=$(ls -t $ART_FOLDER | grep -E "agent-config-[0-9]{4}-[0-9]{2}-[0-9]{2}-[0-9]{2}-[0-9]{2}-[0-9]{2}.json" | head -n 1)
+
+export CONFIG_FILES=$ART_FOLDER/$ARG_FILE
+
+echo $CONFIG_FILES
+
+ls -l $CONFIG_FILES
+
+
+# Start the first process (validator) in the background
+cargo run --release --bin validator -- \
+    --db $DB_VALIDATOR \
+    --originChainName xrpledger \
+    --checkpointSyncer.type localStorage \
+    --checkpointSyncer.path $VALIDATOR_SIGNATURES_DIR \
+    --validator.key $HYP_KEY &
+
+# # Start the second process (relayer) in the background
+cargo run --release --bin relayer -- \
+    --db $DB_RELAYER \
+    --relayChains xrpledger,sepolia \
+    --allowLocalCheckpointSyncers true \
+    --defaultSigner.key $HYP_KEY \
+    --metrics-port 9091 &
+
+
+echo "Both processes have finished."
 
